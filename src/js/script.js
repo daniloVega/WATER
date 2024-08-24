6ce1e9703ed3781c60e48525da04a86c4b94c1ee
danilo.zagarcanin <danilo.zagarcanin@omninext.it>
2024-08-24 16:33:09 +0200

    styled header mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,47 @@
-import "../scss/style.scss";
-// Hamburger menu logic 
-document.getElementById('hamburger').addEventListener('click', function() {
-	this.classList.toggle('open');
-	document.getElementById('menu').classList.toggle('open');
+import '../scss/style.scss';
+
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+const dropdownToggles = document.querySelectorAll('.dropdown__toggle');
+const dropdownLinks = document.querySelectorAll('.dropdown-menu a');
+const menuLinks = document.querySelectorAll(
+  '.menu__list--item a:not(.dropdown__toggle)'
+);
+
+const toggleMenu = (element, className) => element.classList.toggle(className);
+
+hamburger.addEventListener('click', (event) => {
+  event.stopPropagation();
+  toggleMenu(hamburger, 'open');
+  toggleMenu(menu, 'open');
 });
-// Dropdownlogic
-document.querySelectorAll('.dropdown-toggle').forEach(toggle => {
-	toggle.addEventListener('click', function(event) {
-		event.preventDefault(); // Prevent the default action of the link
-		const parentLi = this.parentElement;
-		parentLi.classList.toggle('active'); // Toggle the 'active' class to show/hide the dropdown menu
-	});
+
+dropdownToggles.forEach((toggle) => {
+  toggle.addEventListener('click', (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    const dropdown = toggle.closest('.dropdown');
+    dropdown.classList.toggle('active');
+  });
 });
+
+// Handle dropdown link click
+dropdownLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.stopPropagation();
+    const parentDropdown = link.closest('.dropdown');
+    if (parentDropdown) parentDropdown.classList.remove('active');
+    hamburger.classList.remove('open');
+    menu.classList.remove('open');
+  });
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.stopPropagation();
+    hamburger.classList.remove('open');
+    menu.classList.remove('open');
+  });
+});
+
+menu.addEventListener('click', (event) => event.stopPropagation());
